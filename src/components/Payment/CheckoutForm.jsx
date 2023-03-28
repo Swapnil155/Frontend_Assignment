55eb4118980af1eb674a591585e0a66abcd27b58
Swapnil Bendal <84583651+Swapnil155@users.noreply.github.com>
2023-03-28 19:17:17 +0530

    change a stripe Public key

--- src/components/Payment/CheckoutForm.jsx
+++ src/components/Payment/CheckoutForm.jsx
@@ -11,7 +11,8 @@ import { Container } from "react-bootstrap";
 import UserServices from "../../services/user.services";
 
 const stripePromise = loadStripe(
-  process.env.REACT_APP_KEY
+//   process.env.REACT_APP_KEY
+  "pk_test_51MZrheSDSUOhX3MpsPyM1G4z4IJq5LIjpL8SYBg2FpEB9nXoQLzJh7fELcaQffdZqlbdUqMK56nnmKWen9mfhiWM00Gt1RnFeu"
 );
 
 const Makepayment = () => {
