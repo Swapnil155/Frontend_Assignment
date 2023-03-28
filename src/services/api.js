1cb73c27203a431dddd506f72cceff21fb70c8e9
Swapnil Bendal <84583651+Swapnil155@users.noreply.github.com>
2023-03-28 18:50:14 +0530

    Final changes api changes
    api deployed in render

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,8 @@ import axios from "axios"
 
 const instance = axios.create({
     // baseURL: "http://localhost:5000",
-    baseURL: "https://backend-api-for-assignment-project-sigma.vercel.app",
+//     baseURL: "https://backend-api-for-assignment-project-sigma.vercel.app",
+    baseURL: "https://backend-api-for-assignment.onrender.com",
     headers: {
         "Content-Type": "application/json",
         // 'Content-Type': 'multipart/form-data'
